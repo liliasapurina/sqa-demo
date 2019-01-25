dd2df32f57954fe9f64494ceaa283736ca66ddf9
Lilia Sapurina <yoyxoxo@gmail.com>
2019-01-25 18:32:39 +0300

    Update SmartConfigHolder.java
    SmartConfigHolder update

--- src/main/java/com/sqa/demo/smart_config/SmartConfigHolder.java
+++ src/main/java/com/sqa/demo/smart_config/SmartConfigHolder.java
@@ -3,19 +3,13 @@ package com.sqa.demo.smart_config;
 import com.sqa_days_demo.config.pck.SmartConfig;
 import com.sqa_days_demo.config.pck.SmartConfigProperties;
 
-import static smart_config.SmartConfigSystemKeys.*;
-import static smart_config.SmartConfigSystemValues.*;
-
 public class SmartConfigHolder {
     private static SmartConfig SMART_CONFIG;
 
     public static SmartConfig getSmartConfig() {
         SMART_CONFIG = SmartConfigProperties.getConfig(
-                System.getProperty(INFRA.toString(), GREEN.toString()),
-                System.getProperty(AGENT.toString(), LOCAL.toString()).replaceAll("-", ""),
-                System.getProperty(ENV.toString(), SIT.toString()),
-                System.getProperty(ZONE.toString(), UK.toString()),
-                System.getProperty(LOCATION.toString(), INT.toString())
+                System.getProperty("env", "prod"),
+                System.getProperty("locale", "ru")
         );
         return SMART_CONFIG;
     }
